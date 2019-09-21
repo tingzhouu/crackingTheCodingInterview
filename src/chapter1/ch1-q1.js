922d374a6eedb22592e2c039b5c6231384120487
Ting Zhou <limtingzhou@gmail.com>
2019-09-21 15:46:01 +0800

    initial commit

--- src/chapter1/ch1-q1.js
+++ src/chapter1/ch1-q1.js
@@ -5,5 +5,17 @@ export function hasUniqueCharactersSort(str) {
       return false;
     }
   }
-  return false;
+  return true;
+}
+
+
+export function hasUniqueCharactersSet(str) {
+  let chars = new Set();
+  for (let i = 0; i < str.length; i++) {
+    if (chars.has(str[i])) {
+      return false;
+    }
+    chars.add(str[i]);
+    return true;
+  }
 }
